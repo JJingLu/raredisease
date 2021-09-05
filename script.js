debe67ccf43bb14dcf8a8eca070967fcaf10c16f
LJUNE1999 <40060258+LJUNE1999@users.noreply.github.com>
2021-09-05 11:29:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ Copyright © 2011-2015 Caleb Troughton
 Licensed under the MIT license.
 https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
 */
+
+
+// JS
+
+      
 (function() {
   'use strict'
 
@@ -69,6 +74,106 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
 }())
 ;
 
+
+
+function isInViewport(devide,el) {
+  if (!devide) {
+      devide = 1.3;
+  }
+  var elementTop = el.offset().top;
+  var elementBottom = elementTop + el.outerHeight();
+
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height() / devide;
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+}
+
+$(window).on('load resize scroll', function() {
+  $('[data-tspeed]').each(function() {
+      if (isInViewport(1.3,$(this)) && !$(this).data('type')) {
+          typeIt($(this));
+          $(this).attr('data-type','NO_TYPE');
+      }
+  })
+});
+
+function typeIt(idNode) {
+  var way = $(idNode),
+      currDiv = way.find('[data-typeit]'),
+      times = currDiv.length,
+      speed = Number(way.data('tspeed')),
+      wait = Number(way.data('tawait')),
+      count = 0,
+      countIndex = 0,
+      contentOut = '',
+      skip = false,
+      cursor = '<span class="slash-typeit">|<span>';
+
+  ! function doMagic(idDiv) {
+      var currentData = currDiv.eq(idDiv).data('typeit');
+      currDiv.eq(idDiv).addClass('activated-typeit');
+    
+      ! function letterByLetter() {
+          contentOut += currentData[countIndex];
+
+          if (countIndex == currentData.length - 1) cursor = '';
+
+          var pause = contentOut.substr(contentOut.length - 1);
+
+          if (pause === '|') {
+              var delay = 500;
+              for (var i = countIndex + 1; i < currentData.length; i++) {
+                  if (currentData[i] === '|') {
+                      i = i - countIndex;
+                      delay = currentData.substr(countIndex, i + 1);
+                      currentData = currentData.replace(delay, '');
+                      delay = delay.replace(/\|/g, '');
+                      setTimeout(function() {
+                          typing();
+                      }, skip ? 0 : delay);
+                      return false;
+                  }
+              }
+          } else {
+              typing();
+          }
+
+          function typing() {
+              contentOut = contentOut.replace('|', '').replace(/\€/g, '<i class="break-typeit"></i>');
+         
+              $(currDiv.eq(idDiv)).html('<p>'+contentOut + cursor+'</p>');
+              countIndex++; 
+
+              if (countIndex != currentData.length) {
+                  setTimeout(function() {
+                      letterByLetter();
+                  }, speed);
+              } else if (countIndex == currentData.length && count != times - 1) {
+                  countIndex = 0;
+                  contentOut = '';
+                  count++;
+                  setTimeout(function() {
+                      doMagic(count);
+                  }, wait);
+              }
+          }
+
+      }()
+
+  }(0)
+
+  way.on('click', function() {
+      skip = true;
+      speed = 0;
+      wait = 0;
+  })
+
+}
+
+
+
+
 $(function() { 
 	  $('span.pullquote').each(function() { 
 		var $parentParagraph = $(this).parent('p'); 
@@ -107,3 +212,22 @@ $('#read-more').localScroll();
 $( "#btn-menu" ).click(function() {
   $( "#menu" ).toggleClass( "close" );
 });
+
+
+var Icebergtl = new TimelineMax({});
+Icebergtl.from("#Scenery", 1.5, {autoAlpha: 0, x: 40});
+Icebergtl.staggerFromTo("#Iceberg", 1.5, {y:15},{y:-5, ease: Sine.easeInOut, repeat: -1, yoyo: true})
+
+var LoadingJokestl = new TimelineMax({delay: 2});
+LoadingJokestl.to("#IKnow", 1, {x: 400, autoAlpha: 1})
+.to("#close", 0.5, {scaleY:1, autoAlpha: 1})
+.to("#ISay", 1, {x: 500, autoAlpha: 1})
+.to("#closer", 0.5, {scaleY:1, autoAlpha: 1})
+.to("#YouKnow", 1, {x: 400, autoAlpha: 1})
+.to("#closest", 0.5, {scaleY:1, autoAlpha: 1})
+.to("#Sdras", 1, {x: 480, autoAlpha: 1})
+
+
+
+
+
